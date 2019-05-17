0212fcc92183ce768c66aad8dfb3424a3be49d6f
dimaldo <dimaldo@example.com>
2019-05-17 12:27:40 +0300

    Добавиили цикл. Задание 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-var money = prompt("Ваш бюджет на месяц?",'');
+var money = +prompt("Ваш бюджет на месяц?",'');
 var time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 var appData = {
@@ -11,9 +11,27 @@ var appData = {
     savings: false    
 };
 
-var a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется?", ''); 
+for (let i = 0; i < 2; i++ ){
+    var a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = prompt("Во сколько обойдется?", '');
+    if ((typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b) != null) && a != '' && b != '' && a.length < 50){
+        console.log('done');
+        appData.expenses[a] = b;
+     }else{
 
-appData.expenses.a1 = a2;
-        
-alert(appData.budget/30);
+     }    
+
+};
+     
+appData.moneyPerDay = appData.budget/30;
+alert("Ежедневный бюджет: "+ appData.moneyPerDay);
+
+if(appData.moneyPerDay <100){
+    console.log("Минимальный уровень достатка");
+}else if(appData.moneyPerDay >100 && appData.moneyPerDay <2000){
+    console.log("Средний уровень достатка");
+}else if(appData.moneyPerDay > 2000){
+    console.log("Высокий уровень дохода");
+}else {
+    console.log("Произошла ошибка");
+}
