876b10935fbcd99f04c3dd83aa0d93f8c39586cc
dimaldo <dimaldo@example.com>
2019-05-23 13:37:45 +0300

    Разобрались со скобками

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ var appData = {
 
 for (let i = 0; i < 2; i++ ){
     var a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    b = prompt("Во сколько обойдется?", '');
-    if ((typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b) != null) && a != '' && b != '' && a.length < 50){
-        console.log('done');
+        b = prompt("Во сколько обойдется?", '');
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+        console.log("done");
         appData.expenses[a] = b;
      }else{
-
+        i = i - 1;
      }    
 
 };
